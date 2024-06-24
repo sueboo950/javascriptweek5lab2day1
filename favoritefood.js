5295b3ff32779d3b05a5e0978d88021d9730005a
sophie950 <sueboo950950@gmail.com>
2024-06-23 20:38:24 -0400

    update

--- favoritefood.js
+++ favoritefood.js
@@ -1,1 +1,6 @@
+function favoritefoodChanger() {
+    MyFavoritefoodHeader.innerHTML ="Chocolate";
+    MyFavoritefoodParagraph.innerHTML = "White chocolate is really good.";
 
+}
+MyFavoritefoodButton.onclick = favoritefoodChanger;
